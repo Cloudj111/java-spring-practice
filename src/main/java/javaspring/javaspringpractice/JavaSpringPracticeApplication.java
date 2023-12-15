536064bef0b4d09f8b0140f482518d3549f55576
jmkwon <kjminu96@gmail.com>
2023-12-15 09:15:29 +0900

    init commit

---
+++ src/main/java/javaspring/javaspringpractice/JavaSpringPracticeApplication.java
@@ -0,0 +1,13 @@
+package javaspring.javaspringpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringPracticeApplication.class, args);
+	}
+
+}
