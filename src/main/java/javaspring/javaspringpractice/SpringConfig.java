2d8a9ba5db5989918014bda8222403a4a8965ac9
jmkwon <kjminu96@gmail.com>
2023-12-27 11:37:34 +0900

    Update for JavaSpringExample code at 23-12-27 11:37 ~ 6-6 Spring Data JPA

--- src/main/java/javaspring/javaspringpractice/SpringConfig.java
+++ src/main/java/javaspring/javaspringpractice/SpringConfig.java
@@ -1,6 +1,5 @@
 package javaspring.javaspringpractice;
 
-import jakarta.persistence.EntityManager;
 import javaspring.javaspringpractice.repository.JpaMemberRepository;
 import javaspring.javaspringpractice.repository.MemberRepository;
 import javaspring.javaspringpractice.service.MemberService;
@@ -11,12 +10,22 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SpringConfig {
 
-    private EntityManager em;
+    // SpringDataJpaMemberRepository 에서 자동으로 만들어진 구현체가 여기에 주입됨.
+    private final MemberRepository memberRepository;
 
     @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
+
+
+    // 기존 JPA 코드
+//    private EntityManager em;
+//
+//    @Autowired
+//    public SpringConfig(EntityManager em) {
+//        this.em = em;
+//    }
 
 //    기존 Spring, JDBC 코드
 //    private DataSource dataSource;
@@ -25,17 +34,20 @@ public class SpringConfig {
 //        this.dataSource = dataSource;
 //    }
 
-
+    // 기존에는 memberService 객체를 반환하는 코드였다면,
+    // 변경 후 자동 생성되어 주입된 memberRepository를 사용하게 됨(의존성 주입)
     @Bean
     public MemberService memberService(){
-        return new MemberService(memoryMemberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memoryMemberRepository(){
-//        return new MemoryMemberRepository();                  // # 메모리에 저장하는 코드
-//        return new JdbcTemplateMemberRepository(dataSource);  // # JDBC로 DB 접근해서 사용하는 코드
-        return new JpaMemberRepository(em);                     // # JPA로 DB 접근해서 사용하는 코드
-    }
+    // 기존에 사용하던 기본 JPA 저장소 객체를
+    // SpringDataJpaMemberRepository에서 자동 생성되어 주입되기 때문에 사용하지 않음.
+//    @Bean
+//    public MemberRepository memoryMemberRepository(){
+//        return new MemoryMemberRepository();                    // # 메모리에 저장하는 코드
+//        return new JdbcTemplateMemberRepository(dataSource);    // # JDBC로 DB 접근해서 사용하는 코드
+//        return new JpaMemberRepository(em);                     // # JPA로 DB 접근해서 사용하는 코드
+//    }
 
 }
--- src/main/java/javaspring/javaspringpractice/repository/SpringDataJpaMemberRepository.java
+++ src/main/java/javaspring/javaspringpractice/repository/SpringDataJpaMemberRepository.java
@@ -1,4 +1,14 @@
 package javaspring.javaspringpractice.repository;
 
-public interface SpringDataJpaMemberRepository {
+import javaspring.javaspringpractice.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+// JpaRepository<클래스, 식별자 id의 타입 Long>
+public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
+
+    // JPQL select m from Member m where m.name = ?
+    @Override
+    Optional<Member> findByName(String name);
 }
